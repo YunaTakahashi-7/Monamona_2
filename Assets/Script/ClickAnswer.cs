93383f8e5e83c6655ed9b13464c7bb3d06cdbeea
Yuna Takahashi <y.takahashi99624@gmail.com>
2021-02-07 14:35:23 +0900

    HensyuRandomImage

---
+++ Assets/Script/ClickAnswer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAnswer : MonoBehaviour
+{
+   public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("せいかーい");
+        }else if (Input.GetKeyDown(KeyCode.N))
+        {
+            Destroy(this.gameObject);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Script/TrainScript.cs
+++ Assets/Script/TrainScript.cs
@@ -23,46 +23,46 @@ public class TrainScript : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (Train[2].activeSelf || Train[7].activeSelf)
-            {
-                Debug.Log("maruSSS");
-                score += 10;
-                ScoreText.text = "" + score;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            //Debug.Log("N was pressed.");
-            if (Train[0].activeSelf || Train[1].activeSelf)
-            {
-                Debug.Log("maruNNN");
-                score += 10;
-                ScoreText.text = "" + score;
-                Train[0].SetActive(false);
-                Train[1].SetActive(false);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            //Debug.Log("M was pressed.");
-            if (Train[3].activeSelf || Train[4].activeSelf || Train[6].activeSelf || Train[8].activeSelf)
-            {
-                Debug.Log("maruMMM");
-                score += 10;
-                ScoreText.text = "" + score;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            //Debug.Log("N was pressed.");
-            if (Train[5].activeSelf)
-            {
-                Debug.Log("maruJJJ");
-                score += 10;
-                ScoreText.text = "" + score;
-            }
-        }
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        if (Train[2].activeSelf || Train[7].activeSelf)
+    //        {
+    //            Debug.Log("maruSSS");
+    //            score += 10;
+    //            ScoreText.text = "" + score;
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.N))
+    //    {
+    //        //Debug.Log("N was pressed.");
+    //        if (Train[0].activeSelf || Train[1].activeSelf)
+    //        {
+    //            Debug.Log("maruNNN");
+    //            score += 10;
+    //            ScoreText.text = "" + score;
+    //            Train[0].SetActive(false);
+    //            Train[1].SetActive(false);
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.M))
+    //    {
+    //        //Debug.Log("M was pressed.");
+    //        if (Train[3].activeSelf || Train[4].activeSelf || Train[6].activeSelf || Train[8].activeSelf)
+    //        {
+    //            Debug.Log("maruMMM");
+    //            score += 10;
+    //            ScoreText.text = "" + score;
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.J))
+    //    {
+    //        //Debug.Log("N was pressed.");
+    //        if (Train[5].activeSelf)
+    //        {
+    //            Debug.Log("maruJJJ");
+    //            score += 10;
+    //            ScoreText.text = "" + score;
+    //        }
+    //    }
     }
 }
